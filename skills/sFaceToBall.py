9606a7dea1e35b226187d7ea6191315e78ec12c1
Saurabh Agarwal <shourabh.agarwal@gmail.com>
2018-05-05 17:42:00 +0530

    Working sFaceToBall

--- skills/sFaceToBall.py
+++ skills/sFaceToBall.py
@@ -8,22 +8,27 @@ from navigation_py.wrapperpy import *
 from navigation_py.obstacle import Obstacle
 from math import pi, fabs
 
+def debug(param, state, bot_id):
+    botPos = Vector2D(state.homePos[bot_id].x, state.homePos[bot_id].y)
+    ballPos = Vector2D(state.ballPos.x, state.ballPos.y)
+    print '#'*50
+    print 'In sFaceToBall'
+    print 'Current bot pos: {}, {}'.format(state.homePos[bot_id].x, state.homePos[bot_id].y)
+    print 'Current ball pos: {}, {}'.format(state.ballPos.x, state.ballPos.y)
+    print 'Target alignment: {}'.format(botPos.angle(ballPos))
+    print '#'*50
+
 def execute(param, state, bot_id, pub):
     
+    debug(param,state,bot_id)
+
     botPos = Vector2D(int(state.homePos[bot_id].x), int(state.homePos[bot_id].y))
     ballPos = Vector2D(int(state.ballPos.x), int(state.ballPos.y))
-
     vec = Vector2D()
-    finalslope = ballPos.angle(botPos)
+    finalslope = botPos.angle(ballPos)
     turnAngleLeft = vec.normalizeAngle(finalslope - state.homePos[bot_id].theta)  #Angle left to turn
     omega =  1*(math.pi)*turnAngleLeft *MAX_BOT_OMEGA / (2 * math.pi)  #Speedup turn
-
-    # print "bot_id="+str(bot_id)
-    # print "my_theta="+str(state.homePos[bot_id].theta)
-    # print "Turn Angle Left="+str(turnAngleLeft)
-
     if omega < MIN_BOT_OMEGA and omega > -MIN_BOT_OMEGA:
-        # print(omega, MIN_BOT_OMEGA,"omegaaaaaaaaa")
         if omega < 0:
             omega = -MIN_BOT_OMEGA
         else:
