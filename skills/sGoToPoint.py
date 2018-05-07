b9ac40556711844e92f1e1068650cf866b733a26
sipah00 <shubhamsipah00@gmail.com>
2018-05-07 18:36:03 +0530

    added planner callback test

--- skills/sGoToPoint.py
+++ skills/sGoToPoint.py
@@ -41,99 +41,183 @@ def debug(param, state, bot_id):
 	print 'Distance: {}'.format(dist)
 	print '#'*50
 
-def execute(param,state,bot_id, pub,dribbler = False):
-	debug(param, state, bot_id)
-	obs = Vector_Obstacle()
-	for i in range(0,len(state.homeDetected)):
-		if state.homeDetected[i] and i != bot_id:
-			o = Obstacle()
-			o.x=state.homePos[i].x
-			o.y=state.homePos[i].y
-			o.radius=2.5*BOT_RADIUS
-			obs.push_back(o)
+# def execute(param,state,bot_id, pub,dribbler = False):
+# 	# debug(param, state, bot_id)
+# 	print("int GoToPointP")
 
-	for j in range(0,len(state.awayDetected)):
-		if state.awayDetected[j]:
-			o = Obstacle()
-			o.x=state.awayPos[j].x
-			o.y=state.awayPos[j].y
-			o.radius=2.5*BOT_RADIUS
-			obs.push_back(o)
+# 	obs = Vector_Obstacle()
+# 	for i in range(0,len(state.homeDetected)):
+# 		if state.homeDetected[i] and i != bot_id:
+# 			o = Obstacle()
+# 			o.x=state.homePos[i].x
+# 			o.y=state.homePos[i].y
+# 			o.radius=2.5*BOT_RADIUS
+# 			obs.push_back(o)
+
+# 	for j in range(0,len(state.awayDetected)):
+# 		if state.awayDetected[j]:
+# 			o = Obstacle()
+# 			o.x=state.awayPos[j].x
+# 			o.y=state.awayPos[j].y
+# 			o.radius=2.5*BOT_RADIUS
+# 			obs.push_back(o)
 
 
-	pointPos = Vector2D()
-	pointPos.x = int(param.GoToPointP.x)
-	pointPos.y = int(param.GoToPointP.y)
-	point = Vector2D()
-	nextWP = Vector2D()
-	nextNWP = Vector2D()
+# 	pointPos = Vector2D()
+# 	pointPos.x = int(param.GoToPointP.x)
+# 	pointPos.y = int(param.GoToPointP.y)
+# 	point = Vector2D()
+# 	nextWP = Vector2D()
+# 	nextNWP = Vector2D()
 
-	pathplanner = MergeSCurve()
+# 	pathplanner = MergeSCurve()
 
-	botPos = Vector2D(int(state.homePos[bot_id].x), int(state.homePos[bot_id].y))
+# 	botPos = Vector2D(int(state.homePos[bot_id].x), int(state.homePos[bot_id].y))
 
-	pathplanner.plan(botPos,pointPos,nextWP,nextNWP,obs,len(obs),bot_id,not state.isteamyellow)
+# 	pathplanner.plan(botPos,pointPos,nextWP,nextNWP,obs,len(obs),bot_id,True)
 
-	# Komega = 1.5
-	# Kvel = 1.7
+# 	# Komega = 1.5
+# 	# Kvel = 1.7
 
-	# netDist = pointPos.dist(state.homePos[bot_id])
-	# curDist = nextWP.dist(state.homePos[bot_id])
-	# theta_cov = nextWP.normalizeAngle(nextWP.normalizeAngle(param.GoToPointP.finalSlope) - nextWP.normalizeAngle(state.homePos[bot_id].theta))
-	# theta_cur = theta_cov * curDist/netDist
+# 	# netDist = pointPos.dist(state.homePos[bot_id])
+# 	# curDist = nextWP.dist(state.homePos[bot_id])
+# 	# theta_cov = nextWP.normalizeAngle(nextWP.normalizeAngle(param.GoToPointP.finalSlope) - nextWP.normalizeAngle(state.homePos[bot_id].theta))
+# 	# theta_cur = theta_cov * curDist/netDist
 
-	# v_x = -25*Kvel*curDist*math.sin(theta_cur)/HALF_FIELD_MAXX
-	# v_y = 25*Kvel*curDist*math.cos(theta_cur)/HALF_FIELD_MAXX
-	# omega = Komega*theta_cur
-	# print v_x,",",v_y
-	# skill_node.send_command(pub, state.isteamyellow, bot_id, v_x, v_y, omega, 0, dribbler)
+# 	# v_x = -25*Kvel*curDist*math.sin(theta_cur)/HALF_FIELD_MAXX
+# 	# v_y = 25*Kvel*curDist*math.cos(theta_cur)/HALF_FIELD_MAXX
+# 	# omega = Komega*theta_cur
+# 	# print v_x,",",v_y
+# 	# skill_node.send_command(pub, state.isteamyellow, bot_id, v_x, v_y, omega, 0, dribbler)
 
 
    
-	dist = pointPos.dist(botPos)
-	maxDisToTurn = dist -  DRIBBLER_BALL_THRESH
-	angleToTurn = 0 
-	ballPos = Vector2D(state.ballPos.x, state.ballPos.y)
-	if param.GoToPointP.align == True:
-		angleToTurn = pointPos.normalizeAngle(param.GoToPointP.finalSlope - state.homePos[bot_id].theta)
-	else:
-		angleToTurn = pointPos.normalizeAngle(botPos.angle(ballPos) - state.homePos[bot_id].theta)
-	# angleToTurn = pointPos.normalizeAngle(-math.pi+(pointPos.angle(botPos))-(state.homePos[bot_id].theta))
+# 	dist = pointPos.dist(botPos)
+# 	maxDisToTurn = dist -  DRIBBLER_BALL_THRESH
+# 	angleToTurn = 0 
+# 	ballPos = Vector2D(state.ballPos.x, state.ballPos.y)
+# 	if param.GoToPointP.align == True:
+# 		angleToTurn = pointPos.normalizeAngle(param.GoToPointP.finalSlope - state.homePos[bot_id].theta)
+# 	else:
+# 		angleToTurn = pointPos.normalizeAngle(botPos.angle(ballPos) - state.homePos[bot_id].theta)
+# 	# angleToTurn = pointPos.normalizeAngle(-math.pi+(pointPos.angle(botPos))-(state.homePos[bot_id].theta))
 
-	minReachTime = maxDisToTurn / MAX_BOT_SPEED
-	maxReachTime = maxDisToTurn / MIN_BOT_SPEED
+# 	minReachTime = maxDisToTurn / MAX_BOT_SPEED
+# 	maxReachTime = maxDisToTurn / MIN_BOT_SPEED
 
-	minTurnTime = angleToTurn / MAX_BOT_OMEGA
-	maxTurnTime = angleToTurn / MIN_BOT_OMEGA
+# 	minTurnTime = angleToTurn / MAX_BOT_OMEGA
+# 	maxTurnTime = angleToTurn / MIN_BOT_OMEGA
 
-	speed = 0.0
-	omega = angleToTurn * MAX_BOT_OMEGA / (2 * math.pi)
+# 	speed = 0.0
+# 	omega = angleToTurn * MAX_BOT_OMEGA / (2 * math.pi)
 
-	if angleToTurn:
-		if omega < MIN_BOT_OMEGA and omega > -MIN_BOT_OMEGA:
-			if omega < 0:
-				omega = -MIN_BOT_OMEGA
-			else:
-				omega = MIN_BOT_OMEGA
+# 	if angleToTurn:
+# 		if omega < MIN_BOT_OMEGA and omega > -MIN_BOT_OMEGA:
+# 			if omega < 0:
+# 				omega = -MIN_BOT_OMEGA
+# 			else:
+# 				omega = MIN_BOT_OMEGA
 
-	else:
-		omega = 0.0
+# 	else:
+# 		omega = 0.0
 		
    
-	speed= 2*maxDisToTurn*MAX_BOT_SPEED/(HALF_FIELD_MAXX)
-	if (speed)< 2*MIN_BOT_SPEED:
-		speed=2*MIN_BOT_SPEED
-	if  (speed > MAX_BOT_SPEED):
-		speed=MAX_BOT_SPEED    
+# 	speed= 2*maxDisToTurn*MAX_BOT_SPEED/(HALF_FIELD_MAXX)
+# 	if (speed)< 2*MIN_BOT_SPEED:
+# 		speed=2*MIN_BOT_SPEED
+# 	if  (speed > MAX_BOT_SPEED):
+# 		speed=MAX_BOT_SPEED    
 
-	vec = Vector2D()
-	motionAngle = nextWP.angle(botPos)
-	theta  =  -state.homePos[bot_id].theta + motionAngle
-	if dist < DRIBBLER_BALL_THRESH:
-		if dist < 1*BOT_BALL_THRESH:
-			skill_node.send_command(pub, state.isteamyellow, bot_id, 0, 0, omega, 0, True)
-		else:
-			skill_node.send_command(pub, state.isteamyellow, bot_id, speed * math.sin(-theta), speed * math.cos(-theta), omega, 0, True)
+# 	vec = Vector2D()
+# 	motionAngle = nextWP.angle(botPos)
+# 	theta  =  -state.homePos[bot_id].theta + motionAngle
+# 	if dist < DRIBBLER_BALL_THRESH or True:
+# 		if dist < 1*BOT_BALL_THRESH:
+# 			skill_node.send_command(pub, state.isteamyellow, bot_id, 0, 0, omega, 0, True)
+# 		else:
+# 			# skill_node.send_command(pub, state.isteamyellow, bot_id, 1000,0, 0, 0, True)
+# 			skill_node.send_command(pub, state.isteamyellow, bot_id, speed * math.sin(-theta), speed * math.cos(-theta), omega, 0, True)
 
-	else:
-		skill_node.send_command(pub, state.isteamyellow, bot_id, speed * math.sin(-theta), speed * math.cos(-theta), omega, 0, False)
+# 	else:
+# 		skill_node.send_command(pub, state.isteamyellow, bot_id, speed * math.sin(-theta), speed * math.cos(-theta), omega, 0, False)
+
+
+def execute(param,state,bot_id, pub,dribller = False):
+    obs = Vector_Obstacle()
+    for i in range(0,len(state.homeDetected)):
+        if state.homeDetected[i] and i != bot_id:
+            o = Obstacle()     
+            o.x=state.homePos[i].x
+            o.y=state.homePos[i].y
+            o.radius=3.3*BOT_RADIUS
+            obs.push_back(o)
+
+    for j in range(0,len(state.awayDetected)):
+        if state.awayDetected[j]:
+            o = Obstacle()
+            o.x=state.awayPos[j].x
+            o.y=state.awayPos[j].y
+            o.radius=3.3*BOT_RADIUS
+            obs.push_back(o)
+
+
+    pointPos = Vector2D()
+    pointPos.x = int(param.GoToPointP.x)
+    pointPos.y = int(param.GoToPointP.y)
+    point = Vector2D()
+    nextWP = Vector2D()
+    nextNWP = Vector2D()
+
+    pathplanner = MergeSCurve()
+
+    botPos = Vector2D(int(state.homePos[bot_id].x), int(state.homePos[bot_id].y))
+   
+    pathplanner.plan(botPos,pointPos,nextWP,nextNWP,obs,len(obs),bot_id, True)
+    v = Vector2D()
+    distan = botPos.dist(pointPos)
+    maxDisToTurn = distan 
+    angleToTurn = v.normalizeAngle((param.GoToPointP.finalSlope)-(state.homePos[bot_id].theta))
+
+    minReachTime = maxDisToTurn / MAX_BOT_OMEGA
+    maxReachTime = maxDisToTurn / MIN_BOT_OMEGA
+
+    minTurnTime = angleToTurn / MAX_BOT_OMEGA
+    maxTurnTime = angleToTurn / MIN_BOT_OMEGA
+
+    speed = 0.0
+    omega = 2*angleToTurn * MAX_BOT_OMEGA / (2 * math.pi)                 
+
+    if omega < MIN_BOT_OMEGA and omega > -MIN_BOT_OMEGA:
+        if omega < 0:
+            omega = -MIN_BOT_OMEGA
+        else:
+            omega = MIN_BOT_OMEGA
+
+    from math import exp
+
+    speed= 2*maxDisToTurn*MAX_BOT_SPEED/(HALF_FIELD_MAXX)
+    if (speed)< 2*MIN_BOT_SPEED:
+        speed=2*MIN_BOT_SPEED
+    if  (speed > MAX_BOT_SPEED):
+        speed=MAX_BOT_SPEED    
+  
+    vec = Vector2D()
+
+
+    motionAngle = nextWP.angle(botPos)
+    theta  = motionAngle - state.homePos[bot_id].theta
+    if param.GoToPointP.align == False:
+        if distan < DRIBBLER_BALL_THRESH:
+            if distan < BOT_BALL_THRESH:
+                skill_node.send_command(pub, state.isteamyellow, bot_id, 0, 0, omega, 0,dribller)
+              
+            else:
+                skill_node.send_command(pub, state.isteamyellow, bot_id, speed * math.sin(-theta), speed * math.cos(-theta), omega, 0, dribller)
+        else:
+            skill_node.send_command(pub, state.isteamyellow, bot_id, speed * math.sin(-theta), speed * math.cos(-theta), omega, 0, dribller)
+    else:
+        if distan > BOT_BALL_THRESH:
+            skill_node.send_command(pub, state.isteamyellow, bot_id, speed * math.sin(-theta), speed * math.cos(-theta), omega, 0, dribller)
+        else:
+            skill_node.send_command(pub, state.isteamyellow, bot_id, 0, 0, omega, 0,dribller)
+
