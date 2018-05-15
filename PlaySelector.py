b8fc82dd7393808b2db70833fa274e4c967c6353
Snehal-Reddy <snehalreddyk@gmail.com>
2018-05-16 00:00:19 +0530

    attcker error please check

--- PlaySelector.py
+++ PlaySelector.py
@@ -2,7 +2,6 @@ import sys, os
 import rospy
 from krssg_ssl_msgs.msg import BeliefState
 from krssg_ssl_msgs.msg import gr_Commands
-from krssg_ssl_msgs.msg import Referee
 import threading
 import time
 from std_msgs.msg import Int8
@@ -17,8 +16,11 @@ from tactics import TRDefender
 from tactics import TDTP,TAttacker
 from tactics import TTestIt
 
+from skills import skills_union
 from skills import sStop
-from skills import skill_node
+from skills import sGoToPoint
+from utils.role_selector import *
+
 from plays import pStall
 from plays import DTP_Play
 from plays import pCordinatedPass
@@ -30,13 +32,7 @@ goalie_tac = None
 LDefender_tac = None
 RDefender_tac = None
 cur_goalie = 0
-
-
-
-
-isteamyellow = False
-ref_command = False
-
+prev_attacker = 3
 
 def select_play(state):
     global start_time,pub
@@ -51,20 +47,9 @@ def select_play(state):
     # play_Defence = pDefence.pDefence()
     # play_Defence.execute()
 
-def referee_callback(msg):
-    print "Time: {}, Stage: {}, Command: {}".format(msg.packet_timestamp, msg.stage, msg.command)
-    # global pub, ref_command
-    # if msg.command == 0:
-    #     ref_command = True
-    #     for i in xrange(6):     
-    #         os.environ['bot'+str(i)]=str(start_time)
-    #         skill_node.send_command(pub, isteamyellow, i, 0, 0, 0, 0, False)
-    # else:
-    #     ref_command = False
-    # print ("Received referee message",msg.command)
-    # print ("packet_timestamp ",msg.packet_timestamp)
-    # global ref_play_id
-    # ref_play_id = play_id
+def ref_callback(play_id):
+    global ref_play_id
+    ref_play_id = play_id
 
 def goalKeeper_callback(state):
     global pub,goalie_tac,cur_goalie
@@ -78,11 +63,16 @@ def goalKeeper_callback(state):
 
 
 def attacker_callback(state):
-    global pub
-    attacker_id = 5
+    global pub,prev_attacker
+    # print "bef as"
+    attacker_id = attacker_selector(state)
     param = skills_union.SParam()
-    cur_tactic = TAttacker.TAttacker(attacker_id,state,param)
-    cur_tactic.execute(state,pub)
+    if prev_attacker != attacker_id :
+        sStop.execute(param, state, prev_attacker, pub)
+    else:
+        cur_tactic = TAttacker.TAttacker(attacker_id,state,param)
+        cur_tactic.execute(state,pub)
+    prev_attacker = attacker_id
     print ("attacker : ",attacker_id)
 
 def LDefender_callback(state):
@@ -107,16 +97,15 @@ def RDefender_callback(state):
     RDefender_tac.execute(state,pub)
 
 def planner_callback(state):
-    global ref_command, pub
-    if not  ref_command:
-        # print("incoming planner_callback")
-        bot_id = 0
-        ballPos = Vector2D(state.ballPos.x, state.ballPos.y)
-        botpos = Vector2D(state.homePos[bot_id].x,state.homePos[bot_id].y)
-        # print("dist is ",ballPos.dist(botpos))
-        new = TestTac.TestTac(bot_id,state)
-        new.execute(state,pub)
-        # print(" outgoing planner_callback")
+    print("incoming planner_callback")
+    global pub
+    bot_id = 0
+    ballPos = Vector2D(state.ballPos.x, state.ballPos.y)
+    botpos = Vector2D(state.homePos[bot_id].x,state.homePos[bot_id].y)
+    print("dist is ",ballPos.dist(botpos))
+    new = TestTac.TestTac(bot_id,state)
+    new.execute(state,pub)
+    print(" outgoing planner_callback")
 
 def bs_callback(state):
     global cur_play,start_time
@@ -153,14 +142,13 @@ def main():
         print os.environ.get('bot'+str(i))
 
     pub = rospy.Publisher('/grsim_data', gr_Commands, queue_size=1000)
-    rospy.Subscriber("/ref_data", Referee, referee_callback, queue_size=1000)
     # rospy.Subscriber('/belief_state', BeliefState, bs_callback, queue_size=1000)
     # rospy.Subscriber('/belief_state', BeliefState, goalKeeper_callback, queue_size=1000)
     # rospy.Subscriber('/belief_state', BeliefState, debug_subscriber, queue_size=1000)
     # rospy.Subscriber('/belief_state', BeliefState, LDefender_callback, queue_size=1000)
     # rospy.Subscriber('/belief_state', BeliefState, planner_callback, queue_size=1000)
     # rospy.Subscriber('/belief_state', BeliefState, RDefender_callback, queue_size=1000)
-    # rospy.Subscriber('/belief_state', BeliefState, attacker_callback, queue_size=1000)
+    rospy.Subscriber('/belief_state', BeliefState, attacker_callback, queue_size=1000)
     #rospy.Subscriber('/ref_play', Int8, ref_callback, queue_size=1000)
     rospy.spin()
 
