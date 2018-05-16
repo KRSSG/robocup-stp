c504d5cebcdc1c0483a82b5b25c7f1b79dc796c2
Saurabh Agarwal <shourabh.agarwal@gmail.com>
2018-05-16 13:54:12 +0530

    Add RefereeHalt play

--- plays/Referee/RefPlay.py
+++ plays/Referee/RefPlay.py
@@ -7,7 +7,7 @@ class RefPlay(object):
     tactic_mapping = {"TStop": TStop.TStop,
                       "TMark": TMark.TMark,
                       "TPosition": TPosition.TPosition}
-    def __init__(self, state, tactic, params, publisher):
+    def __init__(self, state,  publisher):
         self.active_robots = 6
         self.role_list = [['' for i in range(2)] for j \
             in range(self.active_robots)]
@@ -15,10 +15,11 @@ class RefPlay(object):
         self.robots = []
         self.state = state
         self.publisher = publisher
+
+    def tactic_instance(self, tactic, params):
         for i in range(self.active_robots):
             self.role_list[i][0] = tactic[i]
             self.role_list[i][1] = params[i]
-    def tactic_instance(self):
         for i in range(self.active_robots):
             self.instances.append(RefPlay.tactic_mapping[self.role_list[i][0]]( \
                 i, self.state, self.role_list[i][1]))
@@ -27,9 +28,7 @@ class RefPlay(object):
         for i in range(self.active_robots):
             self.robots.append(thread(self.instances[i], self.state, \
                 gv,self.publisher))
-        # for i in range(self.active_robots):
-            # self.instances[i].execute(self.state, self.publisher)
-        for i in [0,1,2,3,4,5]:
+        for i in range(self.active_robots):
             self.robots[i].start()
-        for i in [0,1,2,3,4,5]:
+        for i in range(self.active_robots):
             self.robots[i].join()
