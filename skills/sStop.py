58ba47691f061551e01819093c8e445204ef9acf
Saurabh Agarwal <shourabh.agarwal@gmail.com>
2018-05-16 13:50:07 +0530

    Make compatible with gv

--- skills/sStop.py
+++ skills/sStop.py
@@ -1,4 +1,4 @@
 import skill_node
 
-def execute(param, state, bot_id, pub):
+def execute(param, state, bot_id, gv, pub):
 	skill_node.send_command(pub, state.isteamyellow, bot_id, 0, 0, 0, 0, False)
