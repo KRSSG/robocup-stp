b96fbe2009346a0eee7bf2f1f69227c1b5c0b86e
Snehal-Reddy <snehalreddyk@gmail.com>
2018-05-22 18:49:19 +0530

    sSpin corrected without previous attacker trash

--- skills/sSpin.py
+++ skills/sSpin.py
@@ -1,4 +1,4 @@
 import skill_node
 
-def execute(param, state, bot_id ,pub):
-	skill_node.send_command(pub,state.isteamyellow, bot_id, 0, 0, param.SpinP.radPerSec, 0, False)
+def execute(param,state,bot_id,pub):
+	skill_node.send_command(pub , state.isteamyellow , bot_id , 0 , 0 , param.SpinP.radPerSec , 0 , False)
